a10794ef44c749a9dac0a52e19e2d095b66efad3
Michael Sladkov <sabpp@mail.ru>
2023-07-24 20:31:34 +0300

    "for" removed

--- app/js/main.js
+++ app/js/main.js
@@ -16,9 +16,9 @@ filt(1);
 //а также 1 - специальное значение аргумента, используемое при смене значения чекбокса завершения задания
 function filt(x) {
   if (x == "all" || (lastwaschecked == "-1" && x == 1)) {
-    for (i = 0; i < tasks.length; i++) {
-      tasks[i].style.display = "flex";
-    }
+    Array.from(tasks).forEach(function (element, i, arr) {
+      element.style.display = "flex";
+    });
     lastwaschecked = "-1";
     localStorage.setItem('lwc', JSON.stringify(lastwaschecked));
     all_but.classList.add("button_active");
@@ -28,27 +28,27 @@ function filt(x) {
   if (x == "chckd" || (lastwaschecked == "1" && x == 1)) {
     lastwaschecked = "1";
     localStorage.setItem('lwc', JSON.stringify(lastwaschecked));
-    for (i = 0; i < tasks.length; i++) {
-      if (!tasks[i].firstChild.checked) {
-        tasks[i].style.display = "none";
+    Array.from(tasks).forEach(function (element, i, arr) {
+      if (!element.firstChild.checked) {
+        element.style.display = "none";
       }
-      if (tasks[i].firstChild.checked) {
-        tasks[i].style.display = "flex";
+      else {
+        element.style.display = "flex";
       }
-    }
+    });
     all_but.classList.remove("button_active");
     chckd_but.classList.add("button_active");
     unchckd_but.classList.remove("button_active");
   }
   if (x == "unchckd" || (lastwaschecked == "0" && x == 1)) {
-    for (i = 0; i < tasks.length; i++) {
-      if (!tasks[i].firstChild.checked) {
-        tasks[i].style.display = "flex";
+    Array.from(tasks).forEach(function (element, i, arr) {
+      if (element.firstChild.checked) {
+        element.style.display = "none";
       }
-      if (tasks[i].firstChild.checked) {
-        tasks[i].style.display = "none";
+      else {
+        element.style.display = "flex";
       }
-    }
+    });
     lastwaschecked = "0";
     localStorage.setItem('lwc', JSON.stringify(lastwaschecked));
     all_but.classList.remove("button_active");
@@ -57,11 +57,11 @@ function filt(x) {
   }
   if (x == 1) {
     let n = 0;
-    for (i = 0; i < tasks.length; i++) {
-      if (tasks[i].firstChild.checked) {
+    Array.from(tasks).forEach(function (element, i, arr) {
+      if (element.firstChild.checked) {
         n += 1;
       }
-    }
+    });
     counter.innerText = "Невыполненных: " + (tasks.length-n).toString();
   }
 }
@@ -70,32 +70,34 @@ function filt(x) {
 function del(x) {
   x.parentNode.remove();
   let n = 0;
-  for (i = 0; i < tasks.length; i++) {
-    if (tasks[i].firstChild.checked) {
+  Array.from(tasks).forEach(function (element, i, arr) {
+    if (element.firstChild.checked) {
       n += 1;
     }
-  }
+  });
   counter.innerText = "Невыполненных: " + (tasks.length-n).toString();
 }
  
 //Ввод задачи и обновление счетчика
 function new_task() {
-  let div = document.createElement('div');
-  div.className = "task";
-  div.innerHTML = "<input class='task__chckbox' type='checkbox' onClick='filt(1)'>"+"<p class='task__text'>"+inp.value+"</p>"+"<button class='task__del' onClick='del(this)'>Удалить</button>";
-  inp.value = "";
-  if (lastwaschecked == "1") {
-    div.style.display = "none";
-  }
-  task_list.appendChild(div);
-  
-  let n = 0;
-  for (i = 0; i < tasks.length; i++) {
-    if (tasks[i].firstChild.checked) {
-      n += 1;
+  if (inp.value != "") {
+    let div = document.createElement('div');
+    div.className = "task";
+    div.innerHTML = "<input class='task__chckbox' type='checkbox' onClick='filt(1)'>"+"<p class='task__text'>"+inp.value+"</p>"+"<button class='task__del' onClick='del(this)'>Удалить</button>";
+    inp.value = "";
+    if (lastwaschecked == "1") {
+      div.style.display = "none";
     }
+    task_list.appendChild(div);
+    
+    let n = 0;
+    Array.from(tasks).forEach(function (element, i, arr) {
+      if (element.firstChild.checked) {
+        n += 1;
+      }
+    });
+    counter.innerText = "Невыполненных: " + (tasks.length-n).toString();
   }
-  counter.innerText = "Невыполненных: " + (tasks.length-n).toString();
 }
 
 inp.addEventListener("keyup", function(event) {
@@ -110,23 +112,19 @@ inp.addEventListener("focusout", function() {
 //Если есть невыполненные задачи, то отметить их выполненными, иначе отметить все невыполненными
 function checkall_func() {
   let n = 0;
-  for (i = 0; i < tasks.length; i++) {
-    if (tasks[i].firstChild.checked) {
+  Array.from(tasks).forEach(function (element, i, arr) {
+    if (element.firstChild.checked) {
       n += 1;
     }
-  }
+  });
   if (n != tasks.length) {
-    for (i = 0; i < tasks.length; i++) {
-        for (i = 0; i < tasks.length; i++) {
-          tasks[i].firstChild.checked = true;
-    }
-  }
+    Array.from(tasks).forEach(function (element, i, arr) {
+      element.firstChild.checked = true;
+    });
   } else {
-    for (i = 0; i < tasks.length; i++) {
-      for (i = 0; i < tasks.length; i++) {
-        tasks[i].firstChild.checked = false;
-      }
-    }
+    Array.from(tasks).forEach(function (element, i, arr) {
+      element.firstChild.checked = false;
+    });
   }
 //Производим обновление сортировки в соответсвии с обновленными значениями чекбоксов
   filt(1);
